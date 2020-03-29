bde6196a0fd1d18dd91565ccf9bd1d2a0d36a619
Edgar Ortiz <ed.ortizm@gmail.com>
2020-03-29 19:19:43 -0300

    minor changes plus report added

--- models_dust.py
+++ models_dust.py
@@ -11,9 +11,6 @@ import matplotlib.pyplot as plt
 models = [ 'MW3.1_00', 'MW3.1_10', 'MW3.1_20', 'MW3.1_30', 'MW3.1_40', 'MW3.1_50', 'MW3.1_60', 'LMC2_00', 'LMC2_05', 'LMC2_10', 'smc']
 q_PAHs  = [ 0.47, 1.12, 1.77, 2.50, 3.19, 3.90, 4.58, 0.75, 1.49, 2.37, 0.10]
 model_q_dic = {models[i]:q_PAHs for i in range(len(models))}
-umins   = [ 0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.70, 0.80, 1.00, 1.20, 1.50, 2.00, 2.50, 3.00, 4.00, 5.00, 7.00, 8.00, 10.0, 12.0, 15.0, 20.0, 25.0]
-umaxs= [1e2,1e3, 1e4, 1e5, 1e6, 1e7]# I added 1e2
-# Actually the directory structure is quite different in the website
 # ftp://ftp.astro.princeton.edu/draine/dust/irem4/
 
 # Class to read the directory structure and pass the data to arrays withing a dictionary
@@ -181,10 +178,10 @@ class Model:
             return None
         plt.semilogx(lambdas,spectrum) # this looks like an emission spectrum
         # plt.loglog do not, neither plt.semilogy
-        plt.title(self.key_min_max[:-4])
+        plt.title(self.key_min_max[:-4] +'_' + str(self.gamma))
         plt.xlabel('$nm$')
         plt.ylabel('$L_{\lambda}$' +' [W/nm/(kg of H)]')
-        plt.savefig(self.key_min_max[:-4]+'.pdf')
+        plt.savefig(self.key_min_max[:-4]+'_' +str(self.gamma)+ '.pdf')
         plt.show()
     def bolometric(self):
         # it returns the bolometric luminosity
